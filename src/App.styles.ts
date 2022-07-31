830fcf35fc434d2c79ba11a2c4dfc03b4321f08b
Ruben andré <rubandre14@gmail.com>
2022-07-31 23:55:00 +0100

    subindo estrutura base

---
+++ src/App.styles.ts
@@ -0,0 +1,14 @@
+import Styled from "styled-components";
+export const Container = Styled.div`
+  background-color: red;
+  min-height: 100vh;
+`;
+
+export const Left = Styled.div`
+background-color:black;
+max-width:17.7vw;
+min-height: 100vh;
+display:flex;
+flex-direction: column;
+
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as C from "./App.styles";
+import logo from "./assets/logo.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Left>
+        <img
+          src="https://pngset.com/images/influencer-marketing-marketplace-tribe-spotify-logo-white-symbol-text-word-face-transparent-png-2504468.png"
+          alt=""
+          width="170px"
+        />
+      </C.Left>
+    </C.Container>
   );
-}
+};
 
 export default App;
