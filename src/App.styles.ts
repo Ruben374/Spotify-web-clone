1bfbab24ce26c7b7ba605688f6162e5e0ab6e2d5
Ruben andré <rubandre14@gmail.com>
2022-08-02 02:08:49 +0100

    resize panels

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,16 +1,26 @@
 import Styled from "styled-components";
 export const Container = Styled.div`
   background-color: red;
-  min-height: 100vh;
+  display:flex;
+  align-items: center;
+  justify-content: space-between;
+
 `;
 
 export const Left = Styled.div`
 background-color:black;
-width:14.3%;
+width:14.5%;
+min-width:6%;
+max-width:26%;
 min-height: 100vh;
 display:flex;
 flex-direction: column;
 padding:25px;
+resize: horizontal;
+overflow: hidden;
+#list{
+    margin-top:20px;
+}
 img{
     margin-bottom:25px;
 }
@@ -20,11 +30,23 @@ img{
     display:flex;
     align-items: center;
     margin-bottom:13px;
-  
+    flex-wrap:nowrap;
+    flex-direction:row;
+    flex-wrap:nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 }
 .icon{
     color:#fff;
     font-size:30px;
     margin-right:10px;
 }
+`;
+export const Right = Styled.div`
+background-color:blue;
+flex:1;
+min-height: 100vh;
+display:flex;
+padding:25px;
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import React, { useEffect } from "react";
 import * as C from "./App.styles";
 import logo from "./assets/logo.png";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import EqualizerIcon from "@material-ui/icons/Equalizer";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 const App = () => {
   return (
     <C.Container>
-      <C.Left>
+      <C.Left id="teste">
         <a href="#">
           <img src={logo} alt="" width="130px" />
         </a>
@@ -20,7 +23,15 @@ const App = () => {
         <a href="#" className="icons">
           <EqualizerIcon className="icon" /> A tua Bibloteca
         </a>
+        <a href="#" className="icons" id="list">
+          <AddBoxIcon className="icon" /> Criar Lista de reprodução
+        </a>
+        <a href="#" className="icons">
+          <FavoriteIcon className="icon" />
+          Criar Lista de reprodução
+        </a>
       </C.Left>
+      <C.Right>...</C.Right>
     </C.Container>
   );
 };
